7a14f95e52d1b356a15eab75f802668ee15f9ca6
Marc Wenzlawski <llbbbmn.wenzlawski@gmail.com>
2020-05-02 20:45:33 +0000

    updated main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use std::io;
+
 fn main() {
-    println!(
-        "{} of {:b} people know binary, the other half doesn't",
-        1, 2
-    );
+    println!("Guess the number");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
